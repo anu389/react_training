d982effbb267ec35d3f597340fefc053ec02686b
Anu Sharma <anu.sharma@adw.kochartech.com>
2024-01-25 17:32:09 +0530

    asdjv

--- p2/vite-project/src/App.jsx
+++ p2/vite-project/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import FoodList from './components/FoodList'
+import "./App.css";
 
 export default function App() {
   const fooditem = [
-   
+   "Pizza", "Pasta", "Burger", "Fries", "Noodles","Manchurian"
   ];
   
   return (
