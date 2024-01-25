7b2a1a5aa91336bfa10b122a85279a47cc21c9d0
Anu Sharma <anu.sharma@adw.kochartech.com>
2024-01-25 14:54:22 +0530

    gfdufg

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -11,7 +11,6 @@ export default function App() {
     <Title/>
     <AddToDo/>
     <TodoList/>
-    
   </center>
   </div>
   </>
--- vite-project/src/Components/TodoList.jsx
+++ vite-project/src/Components/TodoList.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import TodoItem1 from "./TodoItem1";
+import TodoItem2 from "./TodoItem2";
 
 export default function TodoList() {
   return (
@@ -12,44 +14,8 @@ export default function TodoList() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th className="col-2">1</th>
-            <td className="col-4">Yuvi</td>
-            <td className="col-6">
-              <div className="row">
-                <div className="col-8">
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Asperiores, architecto.
-                  </p>
-                </div>
-                <div className="col-4">
-                  <button type="button" class="btn btn-outline-dark">
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th className="col-2">2</th>
-            <td className="col-4">Anu</td>
-            <td className="col-6">
-              <div className="row">
-                <div className="col-8">
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Asperiores, architecto.
-                  </p>
-                </div>
-                <div className="col-4">
-                  <button type="button" class="btn btn-outline-dark">
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </td>
-          </tr>
+            <TodoItem1/>
+            <TodoItem2/>
         </tbody>
       </table>
     </div>
